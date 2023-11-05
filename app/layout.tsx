75788972cd8de4c188f860b44883065294a17cce
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-11-05 10:03:21 +0100

    adding data and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Jalal | Personal Portfolio",
-  description: "Jalal is a full-stack developer",
+  description: "Jalal is a full-stack developer Trainee",
 };
 
 export default function RootLayout({
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
     </html>
   );
 }
