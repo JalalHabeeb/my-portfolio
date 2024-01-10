612a0840452ea76864fcc4373b0a6cdf2a264977
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-01-10 22:50:57 +0100

    styling the cards

--- components/projects.tsx
+++ components/projects.tsx
@@ -23,15 +23,28 @@ type ProjectProps = (typeof projectsData)[number];
 
 const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
   return (
-    <section>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ul>
-        {tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
-      </ul>
-      <Image src={imageUrl} alt="Projects I worked on" quality={95} />
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last: mb-0">
+      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
+        <h3 className="text-2xk font-semibold">{title}</h3>
+        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          {tags.map((tag, index) => (
+            <li
+              className="bg-black/[0.7] px-3 py-1 text-[0.7 rem] uppercase tracking-wider text-white rounded-full"
+              key={index}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Image
+        src={imageUrl}
+        alt="Projects I worked on"
+        quality={95}
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
+      />
     </section>
   );
 };
