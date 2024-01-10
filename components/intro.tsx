53a851a7100f310f473c296df5e913b5b9aeb158
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-01-10 21:34:38 +0100

    add about section

--- components/intro.tsx
+++ components/intro.tsx
@@ -44,7 +44,7 @@ const Intro = () => {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -53,7 +53,7 @@ const Intro = () => {
         <span className="font-bold"> full-stack developer trainee.</span> I
         enjoy building <span className="italic">sites & apps</span>. My focus is
         <span className="font-bold"> Front-end</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
