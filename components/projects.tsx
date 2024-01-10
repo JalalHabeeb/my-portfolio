39d61d2d38d38c3d8bc0a0155012ae68b7566168
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-01-10 23:41:13 +0100

    move individual project to separate component for use client

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
+import Project from "./project";
 
 const Projects = () => {
   return (
@@ -15,36 +15,6 @@ const Projects = () => {
           </React.Fragment>
         ))}
       </div>
-    </section>
-  );
-};
-
-type ProjectProps = (typeof projectsData)[number];
-
-const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
-  return (
-    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
-        <h3 className="text-2xk font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] px-3 py-1 text-[0.7 rem] uppercase tracking-wider text-white rounded-full"
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <Image
-        src={imageUrl}
-        alt="Projects I worked on"
-        quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 transition group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
-      />
     </section>
   );
 };
