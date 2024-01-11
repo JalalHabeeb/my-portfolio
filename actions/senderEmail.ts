203efc4fb9c1a2b76198fb74dc548d781ef4455b
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-01-12 00:43:35 +0100

    use resend to send email

--- actions/senderEmail.ts
+++ actions/senderEmail.ts
@@ -1,7 +1,24 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-  console.log("running on the server");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const senderEmail = formData.get("email");
+  const message = formData.get("message");
+
+  if (!message || typeof message !== "string") {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "jalalhabeeb@gmail.com",
+    subject: "Message from contact form",
+    reply_to: senderEmail,
+    text: message,
+  });
 };
