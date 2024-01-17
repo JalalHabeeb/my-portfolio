9e65359a71e74afd51b370012bb957995b6ea408
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-01-17 09:56:21 +0100

    update projects

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import myPortfolioImg from "@/public/myPortfolio.jpg";
+import promptopiaImg from "@/public/promptopia.jpg";
+import iconPhotographyImg from "@/public/iconPhotography.jpg";
 
 export const links = [
   {
@@ -62,25 +62,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Portfolio",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "My own portfolio. Contain all my information, Skills, Experiences and Contact details.",
+    tags: ["React", "Next.js", "Tailwind", "TypeScript", "Framer"],
+    imageUrl: myPortfolioImg,
   },
   {
-    title: "rmtDev",
+    title: "Promptopia",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts",
+    tags: ["React", "Next.js", "Tailwind", "Next-auth", "MongoDB"],
+    imageUrl: promptopiaImg,
   },
   {
-    title: "Word Analytics",
+    title: "Icon Photography",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "I'm working on this website for a client who is a photographer and videogrpapher for special occasions.",
+    tags: ["React", "Next.js", "Tailwind"],
+    imageUrl: iconPhotographyImg,
   },
 ] as const;
 
