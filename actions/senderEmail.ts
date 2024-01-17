de5fd55b1c58d34d6cfbcf969c1fc688715664c3
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-01-17 09:16:53 +0100

    add apiKey const

--- actions/senderEmail.ts
+++ actions/senderEmail.ts
@@ -4,7 +4,8 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const apiKey = process.env.RESEND_API_KEY;
+const resend = new Resend(apiKey);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
